c507e34d521e40ddd45501e65d85c7f128a6a9cf
shakib hasan <shakibhasan@gmail.com>
2022-06-03 04:26:14 +0600

    update resume

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -20,34 +20,34 @@ const Resume = () => {
                         <Navbar.Offcanvas
                             id="offcanvasNavbar"
                             aria-labelledby="offcanvasNavbarLabel"
-                            placement="end"
+                            placement="end" 
                         >
-                            <Offcanvas.Header closeButton>
+                            <Offcanvas.Header closeButton >
                                 <Offcanvas.Title id="offcanvasNavbarLabel">
                                     Shakib Hasan
                                 </Offcanvas.Title>
                             </Offcanvas.Header>
-                            <Offcanvas.Body>
+                            <Offcanvas.Body className='offcanvas-body'>
                                 <Nav className="justify-content-center flex-grow-1 ">
 
                                     <div className='wrap5'>
                                         <div className='box5 four5'>
-                                            <Nav.Link href="/" className='navLink' active>Education</Nav.Link>
+                                            <Nav.Link href="/education" className='navLink' active>Education</Nav.Link>
                                         </div>
                                     </div>
                                     <div className='wrap6'>
                                         <div className='box6 four6'>
-                                            <Nav.Link href="/" className='navLink' active>Professional Skills</Nav.Link>
+                                            <Nav.Link href="/skills" className='navLink' active>Professional Skills</Nav.Link>
                                         </div>
                                     </div>
                                     <div className='wrap6'>
                                         <div className='box6 four6'>
-                                            <Nav.Link href="/" className='navLink' active>Experience</Nav.Link>
+                                            <Nav.Link href="/experience" className='navLink' active>Experience</Nav.Link>
                                         </div>
                                     </div>
                                     <div className='wrap6'>
                                         <div className='box6 four6'>
-                                            <Nav.Link href="/" className='navLink' active>Interview</Nav.Link>
+                                            <Nav.Link href="/interview" className='navLink' active>Interview</Nav.Link>
                                         </div>
                                     </div>
                                 </Nav>
@@ -58,40 +58,7 @@ const Resume = () => {
                 </Container>
             </Navbar>
 
-            {/* <Navbar collapseOnSelect expand="lg" className='Navbar' >
-                        <Container >
-                            <Navbar.Brand href="/" className='Logo'>Shakib</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Navbar.Offcanvas
-                                    id="offcanvasNavbar"
-                                    aria-labelledby="offcanvasNavbarLabel"
-                                    placement="start"
-                                >
-                                    <Offcanvas.Header closeButton>
-                                        <Offcanvas.Title id="offcanvasNavbarLabel">
-                                            Shakib Hasan
-                                        </Offcanvas.Title>
-                                    </Offcanvas.Header>
-                                    <Offcanvas.Body>
-                                        <Nav className="justify-content-end flex-grow-1 ">
-                                            <Nav.Link href="/" className='navLink' active>HOME</Nav.Link>
-                                            <Nav.Link href="#Feature" className='navLink'>FEATURES</Nav.Link>
-                                            <Nav.Link href="#portfolio" className='navLink'>PORTFOLIO</Nav.Link>
-                                            <Nav.Link href="#resume" className='navLink'>RESUME</Nav.Link>
-                                            <Nav.Link href="#" className='navLink'>CLIENTS</Nav.Link>
-                                            <Nav.Link href="#" className='navLink'>PRICING</Nav.Link>
-                                            <Nav.Link href="#" className='navLink'>BLOG</Nav.Link>
-                                            <Nav.Link href="#" className='navLink'>CONTACT</Nav.Link>
-                                        </Nav>
-
-                                    </Offcanvas.Body>
-                                </Navbar.Offcanvas>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-           */}
-
+           
         </div>
     );
 };
