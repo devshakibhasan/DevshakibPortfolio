f6adeb2af5776f714d0cdff3ef36bf1c10c262db
shakib hasan <shakibhasan@gmail.com>
2022-06-03 03:30:09 +0600

    update portfolio

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faArrowUp, faArrowUpRightDots, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
@@ -21,7 +21,8 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/gdv5tp5/portfolio-05.webp' className='portfolio-img' />
                                 <h2>Bussinesss Stratagy</h2>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                               <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
+                              
                             </div>
                         </div>
                     </Col>
@@ -31,7 +32,7 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/2PWN6Yh/portfolio-01.webp' className='portfolio-img' />
                                 <h3>MERN Stack Development</h3>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                                <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
                             </div>
                         </div>
                     </Col>
@@ -41,7 +42,7 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/1qGwmT2/portfolio-06.webp' className='portfolio-img' />
                                 <h3>Front-End Development</h3>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                                <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
                             </div>
                         </div>
                     </Col>
@@ -53,7 +54,7 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/v3vd7Ct/portfolio-04.webp' className='portfolio-img' />
                                 <h2>App Development</h2>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                                <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
                             </div>
                         </div>
                     </Col>
@@ -63,7 +64,7 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/gdv5tp5/portfolio-05.webp' className='portfolio-img' />
                                 <h2>Back-End Development</h2>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                                <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
                             </div>
                         </div>
                     </Col>
@@ -73,7 +74,7 @@ const Portfolio = () => {
                                 <img src='https://i.ibb.co/Mnmn2RN/portfolio-03.webp' className='portfolio-img' />
                                 <h2>Flutter Development</h2>
                                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" />
+                                <a href="#" className='iconF'> <FontAwesomeIcon icon={faArrowRight} className="Feature-icons" /> <h5>SEE MORE</h5> </a>
                                 <p className='p'></p>
                             </div>
                         </div>
