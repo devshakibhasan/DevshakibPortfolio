1ecf82405d8f50b91c96f3988ec54043eb9292b4
shakib hasan <shakibhasan@gmail.com>
2022-06-04 04:58:11 +0600

    update resume

--- src/Pages/Resume/ResumeNavbar/ResumeNavbar.js
+++ src/Pages/Resume/ResumeNavbar/ResumeNavbar.js
@@ -40,7 +40,7 @@ const ResumeNavbar = () => {
                                                                         <div class="col-sm-6 py-2"><h6>Degree: <span class="text-secondary">Honours</span></h6></div>
                                                                         <div class="col-sm-6 py-2"><h6>Phone: <span class="text-secondary">+880 1858694456</span></h6></div>
                                                                         <div class="col-sm-6 py-2"><h6>Email: <span class="text-secondary">shakibhasan@gmail.com</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Address: <span class="text-secondary">123 Street, New York, USA</span></h6></div>
+                                                                        <div class="col-sm-6 py-2"><h6>Address: <span class="text-secondary">Siddirganj, Narayan ganj, Bangladesh</span></h6></div>
                                                                         <div class="col-sm-6 py-2"><h6>Freelance: <span class="text-secondary">Available</span></h6></div>
                                                                     </div>
                                                                 </Col>
@@ -164,60 +164,7 @@ const ResumeNavbar = () => {
                                             </Row>
                                         </div>
                                     </Tab>
-                                    <Tab eventKey="Interview" title="Interview">
-                                        <div className="tab-item-wrapper">
-                                            <Row>
-                                                <Col>
-                                                    <div class="wrap8">
-                                                        <div class="box8 four8">
-
-                                                            <h3> Computer Science and Engineering</h3>
-                                                            <p>National University Bangladesh (2019 - 2023)</p>
-                                                            <hr />
-                                                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                                        </div>
-                                                    </div>
-                                                </Col>
-
-                                                <Col>
-                                                    <div class="wrap8">
-                                                        <div class="box8 four8">
-
-
-                                                            <h3>Higher Secondary Certificate</h3>
-                                                            <p>Haziganj Model Govt. College (2016 - 2018)</p>
-                                                            <hr />
-                                                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                                        </div>
-                                                    </div>
-                                                </Col>
-
-                                                <Col>
-                                                    <div class="wrap8 wrapC">
-                                                        <div class="box8 four8">
-
-                                                            <h3>Secondary School Certificate</h3>
-                                                            <p>Ramgonj High School (2016)</p>
-                                                            <hr />
-                                                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                                        </div>
-                                                    </div>
-                                                </Col>
-
-                                                <Col>
-                                                    <div class="wrap8 wrapC">
-                                                        <div class="box8 four8">
-
-                                                            <h3>Primary School Certificate</h3>
-                                                            <p>Darikandi Govt Primary School(2010)</p>
-                                                            <hr />
-                                                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                                                        </div>
-                                                    </div>
-                                                </Col>
-                                            </Row>
-                                        </div>
-                                    </Tab>
+                                  
                                 </Tabs>
                             </div>
                         </div>
