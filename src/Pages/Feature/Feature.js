4e4ef77fe511b825af05942e19c094ed439ea352
shakib hasan <shakibhasan@gmail.com>
2022-06-18 02:14:06 +0600

    feature update

--- src/Pages/Feature/Feature.js
+++ src/Pages/Feature/Feature.js
@@ -70,7 +70,7 @@ const Feature = () => {
                     <Col>
                         <div class="wrap">
                             <div class="box1 four">
-                            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><path className="Feature-iconss3" d="M14.5 10c-.83 0-1.5-.67-1.5-1.5v-5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5z"></path><path className="Feature-iconss3" d="M20.5 10H19V8.5c0-.83.67-1.5 1.5-1.5s1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"></path><path className="Feature-iconss3" d="M9.5 14c.83 0 1.5.67 1.5 1.5v5c0 .83-.67 1.5-1.5 1.5S8 21.33 8 20.5v-5c0-.83.67-1.5 1.5-1.5z"></path><path className="Feature-iconss3" d="M3.5 14H5v1.5c0 .83-.67 1.5-1.5 1.5S2 16.33 2 15.5 2.67 14 3.5 14z"></path><path className="Feature-iconss3" d="M14 14.5c0-.83.67-1.5 1.5-1.5h5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-5c-.83 0-1.5-.67-1.5-1.5z"></path><path className="Feature-iconss3" d="M15.5 19H14v1.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path><path className="Feature-iconss3" d="M10 9.5C10 8.67 9.33 8 8.5 8h-5C2.67 8 2 8.67 2 9.5S2.67 11 3.5 11h5c.83 0 1.5-.67 1.5-1.5z" ></path ><path className="Feature-iconss3" d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z" ></path ></svg>
+                            <img src="https://i.ibb.co/njjgtgH/untitled-3.png" alt="" />
                                 <h2>Flutter Development</h2>
                                 <p>I throw myself down among the tall grass by the stream as I lie close to the earth.</p>
                                 
