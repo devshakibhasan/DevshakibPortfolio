6e802bfb4a1b8b75cea557ba3f9e791e6e206560
shakib hasan <shakibhasan@gmail.com>
2022-06-18 14:22:01 +0600

    create profile section

--- src/Pages/Resume/ResumeNavbar/ResumeNavbar.js
+++ src/Pages/Resume/ResumeNavbar/ResumeNavbar.js
@@ -1,4 +1,6 @@
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faArrowRight, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Nav, Navbar, Button, Container, NavDropdown, Offcanvas, Tabs, Tab, Row, Col } from 'react-bootstrap';
@@ -15,7 +17,7 @@ const ResumeNavbar = () => {
                 <div className="tab-wrapper">
                     <div className="row">
                         <div className="col-lg-12 col-sm-12 ">
-                            <Tabs defaultActiveKey="Profile" className='tabs '>
+                            <Tabs defaultActiveKey="Profile" className='tabs ' >
 
                                 <Tab eventKey="Education" title="Education">
                                     <div className="tab-item-wrapper">
@@ -75,24 +77,100 @@ const ResumeNavbar = () => {
                                                 <div class="wrap9">
                                                     <div class="box9 four9">
 
-                                                        {/* <Row className='rowss'>
-                                                                <Col>
-                                                                    <img src='https://i.ibb.co/K5fvWQ6/shakib2-removebg-preview.png' className='my-img profile-img' alt="" />
-                                                                </Col>
-                                                                <Col>
-                                                                    <h3>MERN Stack Developer</h3>
-                                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tellus est, finibus ut congue sed, faucibus ut dui. Sed congue nisl dolor, id dapibus leo elementum posuere. Ut aliquam metus quis laoreet elementum. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Aliquam porta faucibus arcu, et consequat velit vestibulum in. Donec quis tellus ut urna volutpat posuere quis consectetur quam.</p>
-                                                                    <div class="row mb-3 textT ">
-                                                                        <div class="col-sm-6 py-2"><h6>Name: <span class="text-secondary">Shakib Hasan</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Birthday: <span class="text-secondary">1 July 2000</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Degree: <span class="text-secondary">Honours</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Phone: <span class="text-secondary">+880 1858694456</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Email: <span class="text-secondary">shakibhasan@gmail.com</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Address: <span class="text-secondary">Siddirganj, Narayan ganj, Bangladesh</span></h6></div>
-                                                                        <div class="col-sm-6 py-2"><h6>Freelance: <span class="text-secondary">Available</span></h6></div>
+                                                        <div class="container-fluid " id="about">
+                                                            <div class="container">
+                                                                <div class="position-relative d-flex align-items-center justify-content-center">
+                                                                    <h1 class="display-1 text-uppercase text-white" >About</h1>
+                                                                    <h1 class="position-absolute text-uppercase text-primary">About Me</h1>
+                                                                </div>
+                                                                <div class="row align-items-center">
+                                                                    <div class="col-lg-6 ">
+                                                                        <img class="img-fluid about-img" src="https://i.ibb.co/ynDRm9d/image9-1.jpg" alt="" ></img>
                                                                     </div>
-                                                                </Col>
-                                                            </Row> */}
+                                                                    <div class="col-lg-6">
+                                                                        <h3 class="mb-4">UI/UX Designer and Web Developer</h3>
+                                                                        <p>Tempor eos dolore amet tempor dolor tempor. Dolore ea magna sit amet dolor eirmod. Eos ipsum est tempor dolor. Clita lorem kasd sed ea lorem diam ea lorem eirmod duo sit ipsum. Amet dolor stet lorem diam dolor justo et dolor dolor dolor</p>
+                                                                        <div class="row mb-3 textT ">
+                                                                            <div class="col-sm-6 py-2"><h6>Name: <span class="text-secondary">Kate Winslet</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Birthday: <span class="text-secondary">1 April 1990</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Degree: <span class="text-secondary">Master</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Experience: <span class="text-secondary">10 Years</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Phone: <span class="text-secondary">+012 345 6789</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Email: <span class="text-secondary">info@example.com</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Address: <span class="text-secondary">123 Street, New York, USA</span></h6></div>
+                                                                            <div class="col-sm-6 py-2"><h6>Freelance: <span class="text-secondary">Available</span></h6></div>
+                                                                        </div>
+
+                                                                        <div className='box20'>
+                                                                            <Col>
+                                                                                <div class="wrap20">
+                                                                                    <div class="box22 four">
+
+
+                                                                                        <h2 className='numb'>3+</h2>
+                                                                                        <h2> Years of experiences</h2>
+                                                                                    </div>
+
+
+                                                                                </div>
+                                                                            </Col>
+                                                                            <Col>
+                                                                                <div class="wrap20">
+                                                                                    <div class="box22 four">
+                                                                                        <h2 className='numb'>10+</h2>
+                                                                                        <h2> Completed Projects</h2>
+
+
+                                                                                    </div>
+
+
+                                                                                </div>
+                                                                            </Col>
+                                                                            <Col>
+                                                                                <div class="wrap20">
+                                                                                    <div class="box22 four">
+                                                                                        <h2 className='numb'>20+</h2>
+                                                                                        <h2> Happy Customers</h2>
+
+                                                                                    </div>
+
+
+                                                                                </div>
+                                                                            </Col>
+                                                                            <Col>
+                                                                                <div class="wrap20">
+                                                                                    <div class="box22 four">
+                                                                                    <h2 className='numb'>5+</h2>
+                                                                                        <h2> Awards won</h2>
+
+                                                                                    </div>
+
+
+                                                                                </div>
+                                                                            </Col>
+                                                                        </div>
+
+                                                                        <div className='social'>
+                                                                            <Col className='bottom1'>
+                                                                                <div class="wrap20">
+                                                                                    <div class="btn btn__secondary btn-first"> <FontAwesomeIcon icon={faEnvelope} className="mail" /><p>Hire Me</p></div>
+                                                                                </div>
+                                                                            </Col>
+
+                                                                            <Col className='bottom1 bottom4'>
+                                                                                <div className='socia-link'>
+                                                                                    <div class="wrap20">
+                                                                                        <div class="btn btn__secondary btn-second"><FontAwesomeIcon icon={faDownload} className="mail" /><p>Download CV</p></div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </Col>
+                                                                        </div>
+                                                                        {/* <a href="" class="btn">Hire Me</a>
+                                                                        <a href="" class="btn">Learn More</a> */}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </Col>
