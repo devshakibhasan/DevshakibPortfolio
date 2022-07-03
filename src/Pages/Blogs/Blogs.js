916fc58dc466b4ea881a6064b90aabab3e1788b2
shakib hasan <shakibhasan@gmail.com>
2022-07-03 16:04:48 +0600

    update blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -21,69 +21,68 @@ const Blogs = () => {
             </div>
             <Container className='blogs'>
                 <Row>
-                    <Col>
-                        <div class="wrap13">
-                            <div class="box13 four13">
-                                <div className='img1'>
-                                <img src='https://www.appsflyer.com/wp-content/uploads/2021/10/Feature-image-917x480-2.png' className='portfolio-img' />
-                                </div>
-                                <div className='portfolio-title'>
-                                    <h5>APPLICATION</h5>
-                                    <div className='portfolio-liked'>
-                                        <FontAwesomeIcon icon={faClock} />
-                                        <h6>2 min read</h6>
+                    <Col lg='4' md='6' sm='11' xs='11'>
+                        <div class="portfolioWrap">
+                            <a href="">
+                                <div class="portfolioBox four BlogsBox">
+                                    <div className='portfolio-img'>
+                                        <img src='https://media.wired.com/photos/5f46e666f7889e749ac05577/master/w_2560%2Cc_limit/security-firefox-andriod-app.jpg' className='portfolio-img' />
                                     </div>
+                                    <div className='portfolio-title'>
+                                        <h5>APPLICATION</h5>
+                                        <div className='portfolio-liked'>
+                                            <FontAwesomeIcon icon={faClock} />
+                                            <h6>2 min read</h6>
+                                        </div>
+                                    </div>
+                                    <h2>Mobile app landing design</h2>
+                                    <FontAwesomeIcon icon={faArrowTrendUp} className="portfolio-icons" />
                                 </div>
-
-                                <h2>Mobile app landing design and app maintain</h2>
-                                <FontAwesomeIcon icon={faArrowTrendUp} className="Feature-iconssss" />
-
-                            </div>
+                            </a>
                         </div>
                     </Col>
-                    <Col>
-                        <div class="wrap13">
-                            <div class="box13 four13">
-                                <div className='img1'>
-                                <img src='https://conteudo.imguol.com.br/c/noticias/75/2020/01/25/tinder-namoro-virtual-namora-online-app-paquera-match-1579961052689_v2_4x3.png' className='portfolio-img' />
-                                </div>
-                                <div className='portfolio-title'>
-                                    <h5>DEVELOPMENT</h5>
-                                    <div className='portfolio-liked'>
-                                        <FontAwesomeIcon icon={faClock} />
-                                        <h6>2 min read</h6>
+                    <Col lg='4' md='6' sm='11' xs='11'>
+                        <div class="portfolioWrap">
+                            <a href="">
+                                <div class="portfolioBox four BlogsBox">
+                                    <div className='portfolio-img'>
+                                        <img src='https://media.wired.com/photos/5f46e666f7889e749ac05577/master/w_2560%2Cc_limit/security-firefox-andriod-app.jpg' className='portfolio-img' />
                                     </div>
+                                    <div className='portfolio-title'>
+                                        <h5>DEVELOPMENT</h5>
+                                        <div className='portfolio-liked'>
+                                            <FontAwesomeIcon icon={faClock} />
+                                            <h6>2 min read</h6>
+                                        </div>
+                                    </div>
+                                    <h2>Design for tecnology and services</h2>
+                                    <FontAwesomeIcon icon={faArrowTrendUp} className="portfolio-icons" />
                                 </div>
-
-                                <h2>Design for tecnology and services</h2>
-                                <FontAwesomeIcon icon={faArrowTrendUp} className="Feature-icons" />
-
-                            </div>
+                            </a>
                         </div>
                     </Col>
-                    <Col>
-                        <div class="wrap13">
-                            <div class="box13 four13">
-                            <div className='img1'>
-                                <img src='https://d540vms5r2s2d.cloudfront.net/mad/uploads/mad_blog_5c24bb0a902351545911050.png' className='portfolio-img' />
-                                </div>
-                                <div className='portfolio-title'>
-                                    <h5>Development</h5>
-                                    <div className='portfolio-liked'>
-                                        <FontAwesomeIcon icon={faClock} />
-                                        <h6>2 min read</h6>
+                    <Col lg='4' md='6' sm='11' xs='11'>
+                        <div class="portfolioWrap">
+                            <a href="">
+                                <div class="portfolioBox four BlogsBox">
+                                    <div className='portfolio-img'>
+                                        <img src='https://media.wired.com/photos/5f46e666f7889e749ac05577/master/w_2560%2Cc_limit/security-firefox-andriod-app.jpg' className='portfolio-img' />
                                     </div>
+                                    <div className='portfolio-title'>
+                                        <h5>APPLICATION</h5>
+                                        <div className='portfolio-liked'>
+                                            <FontAwesomeIcon icon={faClock} />
+                                            <h6>2 min read</h6>
+                                        </div>
+                                    </div>
+                                    <h2>Design for tecnology and services</h2>
+                                    <FontAwesomeIcon icon={faArrowTrendUp} className="portfolio-icons" />
                                 </div>
-
-                                <h2>Design for tecnology and services</h2>
-                                <FontAwesomeIcon icon={faArrowTrendUp} className="Feature-icons" />
-
-                            </div>
+                            </a>
                         </div>
                     </Col>
 
-
-
+                
                 </Row>
             </Container>
         </div>
